138d37c18e558a002136e63d4614f0c4f3e3977d
Jason Ubuntu clase <jasonlob528@gmail.com>
2022-09-30 20:19:19 +0200

    Cosas del video .-.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const contenido = document.getElementById('contenido');
+let nombre = "Jason Londoño";
+
+contenido.innerHTML = nombre;
+
+let resultado = "Nombre: "+nombre;
+
+console.log(resultado);
